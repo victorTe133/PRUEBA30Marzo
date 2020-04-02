a0eb9167bb6d1ba919e942e18f3a95565bd7b2dd
Victor Tejeda <62724633+victorTe133@users.noreply.github.com>
2020-04-02 16:07:53 -0600

    Primera modificacion

---
+++ 30 de marzo/src/pkg30/de/marzo/DeMarzo.java
@@ -0,0 +1,11 @@
+
+package pkg30.de.marzo;
+public class DeMarzo {
+    public static void main(String[] args) 
+    {
+        System.out.println("Nombre: Victor Daniel Tejeda Hernández");
+       System.out.println("Direccion: Huehuetenango Zona 8");
+       System.out.println("Telefono: 34175073");
+    }
+    
+}
